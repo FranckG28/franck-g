e5a6f98bf43ad40d1dd56a7359b26b3c5dc40d66
Franck <gutmann.franck@outlook.fr>
2023-10-15 17:48:54 +0200

    feat(navigation): active link

--- components/shared/NavigationBar.tsx
+++ components/shared/NavigationBar.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames'
 import { Navigation } from 'lib/models/navigation'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useCallback } from 'react'
 
 export default function NavigationBar({
   items,
@@ -9,13 +11,23 @@ export default function NavigationBar({
   items: Navigation[]
   className?: string
 }) {
+  const router = useRouter()
+
+  const isActive = useCallback(
+    (href: string) => {
+      if (href === '/') return router.pathname === '/'
+      return router.pathname.startsWith(href)
+    },
+    [router],
+  )
+
   return (
     <>
       {items && items.length > 0 && (
         <nav
           className={classNames(
             `bg-zinc-700 border-t border-zinc-600 shadow-xl rounded-full
-          flex items-center justify-center gap-8 py-2 px-6`,
+          flex items-center justify-center gap-6 py-2 px-4`,
             className,
           )}
         >
@@ -24,10 +36,19 @@ export default function NavigationBar({
               className="group relative flex flex-col items-start"
               key={index}
             >
-              <p className="text-zinc-100 font-medium text-base">
-                <div className="absolute -inset-x-4 -inset-y-2 z-0 scale-95 bg-zinc-400/20 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 rounded-full"></div>
+              <p
+                className={classNames(
+                  'font-medium text-sm',
+                  isActive(item.href) ? 'text-blue-300' : 'text-zinc-200',
+                )}
+              >
+                <div className="absolute -inset-x-4 -inset-y-2 z-0 scale-95 bg-zinc-400/10 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 rounded-full"></div>
                 <Link href={item.href}>
-                  <span className="absolute -inset-x-4 -inset-y-2 z-20 sm:-inset-x-6 rounded-full"></span>
+                  <span className="absolute -inset-x-4 -inset-y-2 z-20 sm:-inset-x-6 rounded-full">
+                    {isActive(item.href) && (
+                      <span className="absolute inset-x-1 bottom-px h-px bg-gradient-to-r from-teal-500/0 via-blue-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-blue-400/40 dark:to-teal-400/0"></span>
+                    )}
+                  </span>
                   <span className="relative z-10">{item.name}</span>
                 </Link>
               </p>
