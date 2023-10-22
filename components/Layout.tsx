4212e5b89447879556ae1af1c636e507f63ea490
Franck <gutmann.franck@outlook.fr>
2023-10-22 18:56:09 +0200

    style: decrease footer margin

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,32 +19,30 @@ export default function Layout({
   className?: string
 }) {
   return (
-    <>
-      <div className="min-h-screen relative">
-        <div className="fixed inset-0 flex justify-center sm:px-8 -z-10">
+    <div className="min-h-screen relative">
+      <div className="fixed inset-0 flex justify-center sm:px-8 -z-10">
+        <div className="flex w-full max-w-7xl lg:px-8">
+          <div className="w-full ring-1 bg-zinc-900 ring-zinc-300/20"></div>
+        </div>
+      </div>
+      <AlertBanner preview={preview} loading={loading} />
+      <TopBar settings={settings} />
+      <main
+        className={classNames(
+          'flex flex-col justify-center mt-28 lg:mt-36',
+          className,
+        )}
+      >
+        {children}
+      </main>
+      <footer>
+        <div className="mt-12 flex justify-center sm:px-8">
           <div className="flex w-full max-w-7xl lg:px-8">
-            <div className="w-full ring-1 bg-zinc-900 ring-zinc-300/20"></div>
+            <div className="w-full h-px bg-zinc-300/20"></div>
           </div>
         </div>
-        <AlertBanner preview={preview} loading={loading} />
-        <TopBar settings={settings} />
-        <main
-          className={classNames(
-            'flex flex-col justify-center mt-28 lg:mt-36',
-            className,
-          )}
-        >
-          {children}
-        </main>
-        <footer>
-          <div className="mt-16 flex justify-center sm:px-8">
-            <div className="flex w-full max-w-7xl lg:px-8">
-              <div className="w-full h-px bg-zinc-300/20"></div>
-            </div>
-          </div>
-          <Footer settings={settings} />
-        </footer>
-      </div>
-    </>
+        <Footer settings={settings} />
+      </footer>
+    </div>
   )
 }
