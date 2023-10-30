9186ccc4e4704e2e31149af9247fc40fc4688cc2
Franck G <gutmann.franck@outlook.fr>
2023-10-31 00:38:24 +0100

    style(image): align caption left & improve spacing

--- components/shared/SanityImage.tsx
+++ components/shared/SanityImage.tsx
@@ -21,10 +21,10 @@ export const SanityImage = (props: Props) => {
         {...imageProps}
         alt={alt}
         sizes="(max-width: 800px) 100vw, 800px"
-        className="rounded-lg"
+        className="rounded-lg mt-4"
       />
       {caption && (
-        <figcaption className="mt-2 text-center italic text-sm text-zinc-500 dark:text-zinc-400">
+        <figcaption className="mt-2 text-left italic text-sm text-zinc-500 dark:text-zinc-400">
           {caption}
         </figcaption>
       )}
