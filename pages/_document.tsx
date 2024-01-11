8e7ebef12d09f3f5d041e68f19571b1388de4bef
Franck <gutmann.franck@outlook.fr>
2024-01-11 19:21:51 +0100

    build: add speedinsights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -6,6 +7,7 @@ export default function Document() {
       <Head />
       <body className="bg-black text-white antialiased">
         <Main />
+        <SpeedInsights />
         <NextScript />
       </body>
     </Html>
