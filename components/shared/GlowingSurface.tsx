c9fac3481f3feffb41cf2984189ef823a0232336
Franck <gutmann.franck@outlook.fr>
2024-01-17 17:28:43 +0100

    fix(GlowingSurface): glowing center

--- components/shared/GlowingSurface.tsx
+++ components/shared/GlowingSurface.tsx
@@ -4,6 +4,9 @@ import { MousePosition, useMouse } from '@uidotdev/usehooks'
 import classNames from 'classnames'
 import { RefObject } from 'react'
 
+const xOffset = 24
+const yOffset = 24
+
 export default function GlowingSurface({
   children,
   size = 800,
@@ -23,7 +26,11 @@ export default function GlowingSurface({
           className,
         )}
         style={{
-          background: `radial-gradient(circle ${size}px at ${mouse.elementX}px ${mouse.elementY}px, rgba(63,63,70, 0.4) 0%, rgba(63,63,70, 0.2) 25%, rgba(63,63,70, 0.1) 100%)`,
+          background: `radial-gradient(circle ${size}px at ${
+            mouse.elementX + xOffset
+          }px ${
+            mouse.elementY + yOffset
+          }px, rgba(63,63,70, 0.4) 0%, rgba(63,63,70, 0.2) 25%, rgba(63,63,70, 0.1) 100%)`,
         }}
       ></div>
       {children}
